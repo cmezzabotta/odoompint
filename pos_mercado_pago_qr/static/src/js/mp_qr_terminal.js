ed4c14c6eb3067f61918022bb1dee902283d198a
cmezzabotta <cmmezzabotta@gmail.com>
2025-11-12 01:29:03 -0300

    Delete pos_mercado_pago_qr directory

--- pos_mercado_pago_qr/static/src/js/mp_qr_terminal.js
+++
@@ -1,9 +0,0 @@
-/** @odoo-module **/
-
-import { PaymentTerminal } from "point_of_sale.models";
-
-const MercadoPagoQR = {
-    name: "mercado_pago_qr",
-};
-
-PaymentTerminal.register_terminal(MercadoPagoQR);
