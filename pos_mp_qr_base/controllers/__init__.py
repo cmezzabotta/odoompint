2af0478d93f065f81f1f87ed59a8ec9dcecf3271
cmezzabotta <cmmezzabotta@gmail.com>
2025-11-13 21:34:03 -0300

    Delete pos_mp_qr_base directory

--- pos_mp_qr_base/controllers/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import mp_qr
--- pos_mp_qr_base/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_config_settings
-from . import pos_payment_method
-from . import pos_mp_transaction
-from . import pos_payment
-from . import pos_order
