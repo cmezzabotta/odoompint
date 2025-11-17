ad909fb21ba5df87c96431ac0a66841620ec3e09
cmezzabotta <cmmezzabotta@gmail.com>
2025-11-17 15:33:23 -0300

    Improve POS fiscal ticket handling

--- pos_mezztt_fiscal_ticket/__manifest__.py
+++ pos_mezztt_fiscal_ticket/__manifest__.py
@@ -1,8 +1,9 @@
 {
     'name': 'POS Fiscal Ticket (Mezztt)',
-    'version': '1.0',
+    'version': '18.0.1.0.0',
     'category': 'Point of Sale',
-    'summary': 'Fiscal ticket',
+    'summary': 'Fiscal ticket printing with AFIP data and automatic invoicing.',
+    'license': 'LGPL-3',
     'depends': ['point_of_sale', 'l10n_ar_afipws_fe'],
     'data': [
         'views/report_invoice_fiscal.xml',
